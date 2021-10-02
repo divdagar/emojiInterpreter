15490c68f93f9f0d1d38991f10ef39d547b7d4a6
divdagar <divyanshu.dagar@clarisights.com>
2021-10-02 23:35:41 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+const emojiDictionary = {
+  "😊": "smiling",
+  "😢": "sad",
+  "😠": "angry",
+  "😲": "surprised",
+  "🤡": "clown",
+  "🥳": "celebrating",
+  "👽": "alien",
+  "😂": "laughing",
+  "😴": "sleepy",
+  "🤯": "mind-blown"
+};
+
+const emojisWeKnow = Object.keys(emojiDictionary);
+
+export default function App() {
+  const [emojiMeaning, setEmojiMeaning] = useState("?");
+
+  const emojiInputHandler = (event) => {
+    let userInput = event.target.value;
+
+    if (userInput in emojiDictionary) {
+      setEmojiMeaning(emojiDictionary[userInput]);
+    } else {
+      setEmojiMeaning("Doesn't Exist");
+    }
+  };
+
+  const emojiClick = (emoji) => {
+    setEmojiMeaning(emojiDictionary[emoji]);
+  };
+
+  const emofun = (emoji) => {
+    return <span onClick={() => emojiClick(emoji)}>{emoji}</span>;
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>inside out</h1>
+        <input onChange={emojiInputHandler}></input>
+        <p className="output"> {emojiMeaning} </p>
+        <p className="collection">{emojisWeKnow.map(emofun)}</p>
+      </div>
+    </div>
+  );
+}
